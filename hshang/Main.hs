4ec3b1cbfef3f7279a1f5bc0239ccc438918fd50
jecaro <jeancharles.quillet@gmail.com>
2018-10-20 20:42:35 +0200

    Decrement count only on error

--- hshang/Main.hs
+++ hshang/Main.hs
@@ -2,18 +2,17 @@ module Main where
 
 -- TODO 
 -- Change string based answer for type
--- Decrement count trial only on errors
 -- Remove non alpha characters
 
 -- For random numbers
-import System.Random
-import System.Environment
+import           System.Random
+import           System.Environment
 -- For when
-import Control.Monad.State
+import           Control.Monad.State
 -- For text -> Int conversion
-import Text.Read
+import           Text.Read
 -- For Exception handling
-import Control.Exception
+import           Control.Exception
 
 -- Convert the secret word to the form -x-y---
 guess :: String -> String -> String
@@ -21,46 +20,42 @@ guess word chars = map (\x -> if x `elem` chars then x else '-') word
 
 -- Play loop
 play :: String -> String -> Int -> IO ()
+play secret letters 0     = putStrLn "No ! You've lost"
 play secret letters count = do
+  -- Show the current number of trials
+  putStrLn $ "Still " ++ show count ++ " trials"
 
-    -- Show the current number of trials
-    putStrLn $ "Still " ++ show count ++ " trials"
+  -- Get the char
+  c <- getChar
+  putStrLn ""
 
-    -- Get the char
-    c <- getChar
-    putStrLn ""
+  if c `elem` letters
+    then do
+      putStrLn "You already tried this letter !"
+      play secret letters count
+    else do
+      -- Update number of letters
+      let letters' = letters ++ [c]
 
-    -- Update number of letters
-    let letters' = letters ++ [c]
+      -- The hint word
+      let guess' = guess secret letters'
+      putStrLn guess'
 
-    -- The hint word
-    let guess' = guess secret letters'
-    putStrLn guess'
-
-    -- Updated remaining turns
-    let count' = count - 1
-
-    if count' == 0
-
-        then putStrLn "No ! You've lost"
-
-        else if c `elem` letters
-            then do
-                putStrLn "Already tried !"
-                play secret letters count'
-            else if guess' == secret
-                 then putStrLn "You find it !"
-                 else play secret letters' count'
+      if guess' == secret
+        then putStrLn "You find it !"
+        else do
+            let count' = if c `elem` secret then count else count - 1 
+            play secret letters' count'
 
 -- Answer to the question would play again ?
 getAnswer :: IO String
 getAnswer = do
-    cmd <- getLine
-    if cmd `elem` ["yes", "no"]
-        then return cmd
-        else do
-            putStrLn "I did not understand"
-            getAnswer
+  cmd <- getLine
+  if cmd `elem` ["yes", "no"]
+    then return cmd
+    else do
+      putStrLn "I did not understand"
+      getAnswer
 
 -- Clean up a string at the beginning and the end
 sanitize :: String -> String
@@ -69,57 +64,57 @@ sanitize w = takeWhile (/= ' ') $ dropWhile (== ' ') w
 -- Show the usage message
 usage :: IO ()
 usage = do
-    progName <- getProgName
-    putStrLn $ "Usage: " ++ progName ++ " dictFile nbTrials"
+  progName <- getProgName
+  putStrLn $ "Usage: " ++ progName ++ " dictFile nbTrials"
 
 -- Test the arguments
 validateArgs :: [String] -> Maybe (String, Int)
-validateArgs [x, y] = do 
-    count <- readMaybe y 
-    return (x, count)
+validateArgs [x, y] = do
+  count <- readMaybe y
+  return (x, count)
 validateArgs _ = Nothing
 
 -- Higher level loop
 startPlay :: [String] -> Int -> IO ()
-startPlay words count = do 
-    
-    gen <- newStdGen
-    let (val, _) = randomR (0, length words - 1) gen :: (Int, StdGen)
-        chosen = words !! val
+startPlay words count = do
 
-    putStrLn "Find the secret word ?"
-    
-    play chosen [] count
-    
-    putStrLn "Another game ?"
-    
-    -- Getting answer
-    answer <- getAnswer
-    when (answer /= "no") main
+  gen <- newStdGen
+  let (val, _) = randomR (0, length words - 1) gen :: (Int, StdGen)
+      chosen   = words !! val
+
+  putStrLn "Find the secret word ?"
+
+  play chosen [] count
+
+  putStrLn "Another game ?"
+
+  -- Getting answer
+  answer <- getAnswer
+  when (answer /= "no") main
 
 main :: IO ()
 main = do
 
-    args <- getArgs
+  args <- getArgs
 
-    case validateArgs args of 
+  case validateArgs args of
 
-        Nothing -> usage
+    Nothing                -> usage
 
-        Just (fileName, count) -> do
-            
-            -- Read dictionary
-            dictOrExc <- try (readFile fileName) :: IO (Either SomeException String)
-            
-            case dictOrExc of 
-                
-                Left except -> print except
-                
-                Right dict -> do
+    Just (fileName, count) -> do
 
-                    -- Clean up what we read in the file
-                    let words = filter (not . null) $ map sanitize $ lines dict
-                    if null words
-                        then putStrLn "Empty dictionary"
-                        else startPlay words count
+        -- Read dictionary
+      dictOrExc <- try (readFile fileName) :: IO (Either SomeException String)
+
+      case dictOrExc of
+
+        Left  except -> print except
+
+        Right dict   -> do
+
+            -- Clean up what we read in the file
+          let words = filter (not . null) $ map sanitize $ lines dict
+          if null words
+            then putStrLn "Empty dictionary"
+            else startPlay words count
 
