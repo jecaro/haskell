26406e695305e3cf64f1102ee331e0cfa5ea0ce0
jecaro <jeancharles.quillet@gmail.com>
2018-11-27 17:50:21 +0100

    Refactoring to use StateT

--- hshang/Main.hs
+++ hshang/Main.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
+-- TODO 
+-- show hint at start
+-- separate game logic in module
 
 -- For random numbers
 import           System.Random
@@ -6,17 +13,23 @@ import           System.Environment
 -- For when
 import           Control.Monad.State
 -- For text -> Int conversion
-import           Text.Read
+import qualified Text.Read                     as T
 -- For Exception handling
 import           Control.Exception
+import           Lens.Micro.Platform
+
+-- Simple data type to handle an answer
+data Answer = Yes | No 
+    deriving Eq
+
+data GameState = GameState { _secret  :: String
+                           , _letters :: String
+                           , _count   :: Int}
+makeLenses ''GameState
 
 -- List of allowable chars
 alpha :: String
 alpha = ['a'..'z'] ++ ['A'..'Z']
-
--- Convert the secret word to the form -x-y---
-hint :: String -> String -> String
-hint word chars = map (\x -> if x `elem` chars then x else '-') word
 
 -- Loop until the user send actual char
 getAlphaChar :: IO Char
@@ -30,38 +43,78 @@ getAlphaChar = do
             putStrLn "This character is not allowed"
             getAlphaChar    
 
--- Play loop
-play :: String -> String -> Int -> IO ()
-play secret letters 0     = putStrLn "No ! You've lost"
-play secret letters count = do
-  -- Show the current number of trials
-  putStrLn $ "Still " ++ show count ++ " trials"
+-- Convert the secret word to the form -x-y---
+getHint :: MonadState GameState m => m String
+getHint = do
+  word    <- getSecret
+  letters <- getLetters
+  return $ map (\x -> if x `elem` letters then x else '-') word
 
-  -- Get the char
-  c <- getAlphaChar
-  putStrLn ""
+getCount :: MonadState GameState m => m Int
+getCount = do
+  GameState { _count = count } <- get
+  return count
 
-  if c `elem` letters
-    then do
-      putStrLn "You already tried this letter !"
-      play secret letters count
-    else do
-      -- Update number of letters
-      let letters' = letters ++ [c]
+getLetters :: MonadState GameState m => m String
+getLetters = do
+  GameState { _letters = letters } <- get
+  return letters
 
-      -- The hint word
-      let current = hint secret letters'
-      putStrLn current
+getSecret :: MonadState GameState m => m String
+getSecret = do
+  GameState { _secret = secret } <- get
+  return secret
 
-      if current == secret
-        then putStrLn "You find it !"
-        else do
-            let count' = if c `elem` secret then count else count - 1 
-            play secret letters' count'
+addChar :: MonadState GameState m => Char -> m ()
+addChar c = do
+  gs <- get
+  let update = if c `elem` (gs ^. secret) then id else flip (-) 1
+  put $ gs & count %~ update & letters %~ ([c] ++)
 
--- Simple data type to handle an answer
-data Answer = Yes | No 
-    deriving Eq
+data Status = Won | Lost | Continue
+
+getStatus :: MonadState GameState m => m Status
+getStatus = do 
+  count <- getCount
+  if count == 0 
+  then return Lost 
+  else do 
+    hint <- getHint
+    secret <- getSecret
+    if secret == hint 
+    then return Won
+    else return Continue
+
+play :: StateT GameState IO ()
+play = do
+  status <- getStatus 
+  case status of
+    Won -> liftIO $ putStrLn "You find it !"
+    Lost -> liftIO $ putStrLn "No ! You've lost"
+    Continue -> do 
+      count <- getCount
+      -- Show the current number of trials
+      liftIO $ putStrLn $ "Still " ++ show count ++ " trials"
+
+      -- Get the char
+      c <- liftIO getAlphaChar
+      liftIO $ putStrLn ""
+
+      letters <- getLetters
+      if c `elem` letters
+      then do
+        liftIO $ putStrLn "You already tried this letter !"
+        play 
+      else do
+        -- Update number of letters
+        addChar c
+
+        secret <- getSecret
+        -- The hint word
+        hint <- getHint 
+        liftIO $ putStrLn hint
+
+        play 
 
 -- Convert a string to an answer
 strToAnswer :: String -> Maybe Answer
@@ -92,7 +145,7 @@ usage = do
 -- Test the arguments
 validateArgs :: [String] -> Maybe (String, Int)
 validateArgs [x, y] = do
-  count <- readMaybe y
+  count <- T.readMaybe y
   return (x, count)
 validateArgs _ = Nothing
 
@@ -106,7 +159,7 @@ startPlay words count = do
 
   putStrLn "Find the secret word !"
 
-  play chosen [] count
+  runStateT play (GameState chosen [] count)
 
   putStrLn "Another game ?"
 
